012ab7cc2756efe64c6024f8ebd738e7df5b930a
Md Kutub Uddin <mdkutub150@gmail.com>
2024-12-13 20:02:09 +0600

    Update PostActivity.java

--- Logic_Todo_App/app/src/main/java/com/kutubuddin/logic_todo_app/PostActivity.java
+++ Logic_Todo_App/app/src/main/java/com/kutubuddin/logic_todo_app/PostActivity.java
@@ -3,8 +3,6 @@ package com.kutubuddin.logic_todo_app;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
