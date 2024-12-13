027742f6b02e8f0695053d7407e99f7f5a2823d5
Md Kutub Uddin <mdkutub150@gmail.com>
2024-12-13 16:27:04 +0600

    Update Home.java

--- Logic_Todo_App/app/src/main/java/com/kutubuddin/logic_todo_app/Home.java
+++ Logic_Todo_App/app/src/main/java/com/kutubuddin/logic_todo_app/Home.java
@@ -3,13 +3,13 @@ package com.kutubuddin.logic_todo_app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Home extends AppCompatActivity {
 
-    private Button btnView, btnCreate;
+    private ImageView btnView, btnCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
