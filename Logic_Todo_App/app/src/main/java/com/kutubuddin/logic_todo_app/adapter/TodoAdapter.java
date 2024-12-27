fdcfe28d3a651b027334f46e32e212c37890af96
Md Kutub Uddin <mdkutub150@gmail.com>
2024-12-27 19:13:57 +0600

    Update TodoAdapter.java

--- Logic_Todo_App/app/src/main/java/com/kutubuddin/logic_todo_app/adapter/TodoAdapter.java
+++ Logic_Todo_App/app/src/main/java/com/kutubuddin/logic_todo_app/adapter/TodoAdapter.java
@@ -1,6 +1,5 @@
 package com.kutubuddin.logic_todo_app.adapter;
 
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,49 +14,16 @@ import com.kutubuddin.logic_todo_app.model.TodoModel;
 
 import java.util.List;
 
-public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.ViewHolder>{
+public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.ViewHolder> {
 
     private List<TodoModel> todoList;
     private Context context;
 
-    public TodoAdapter(List<TodoModel> todoList, Context context) {
-        this.todoList = todoList;
-        this.context = context;
-    }
 
-    @NonNull
-    @Override
-    public TodoAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-
-        View view = LayoutInflater.from(context).inflate(com.kutubuddin.logic_todo_app.R.layout.todo_item, parent, false);
-        return new ViewHolder(view);
-
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull TodoAdapter.ViewHolder holder, int position) {
-
-        TodoModel todo = todoList.get(position);
-        holder.textTitle.setText(todo.getTitel());
-        holder.textDescription.setText(todo.getDescription());
-        holder.textTodotype.setText(todo.getTodotype());
-        holder.textPriority.setText(todo.getPriority());
-        holder.textDate.setText(todo.getDate());
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return todoList.size();
-    }
-
+    // ViewHolder to hold references to views in each item
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        TextView textDate;
-        TextView textDescription;
-        TextView textTitle;
-        TextView textTodotype;
-        TextView textPriority;
+        TextView textTitle, textDescription, textTodotype, textPriority, textDate;
+        View deleteButton;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -66,9 +32,49 @@ public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.ViewHolder>{
             textTodotype = itemView.findViewById(R.id.todoType);
             textPriority = itemView.findViewById(R.id.priority);
             textDate = itemView.findViewById(R.id.date);
+            deleteButton = itemView.findViewById(R.id.deleteButton);
         }
-
-
     }
 
+
+
+    private OnDeleteClickListener onDeleteClickListener;
+
+    @Override
+    public int getItemCount() {
+        return todoList.size();
+    }
+
+    // Interface for handling delete button click events
+    public interface OnDeleteClickListener {
+        void onDeleteClick(int id);
+    }
+    // Constructor with listener for delete action
+    public TodoAdapter(List<TodoModel> todoList, Context context, OnDeleteClickListener onDeleteClickListener) {
+        this.todoList = todoList;
+        this.context = context;
+        this.onDeleteClickListener = onDeleteClickListener;
+    }
+
+    @NonNull
+    @Override
+    public TodoAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Inflate the item layout for each Todo item
+        View view = LayoutInflater.from(context).inflate(R.layout.todo_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // Bind data to the views for each Todo item
+        TodoModel todo = todoList.get(position);
+        holder.textTitle.setText(todo.getTitel());
+        holder.textDescription.setText(todo.getDescription());
+        holder.textTodotype.setText(todo.getTodotype());
+        holder.textPriority.setText(todo.getPriority());
+        holder.textDate.setText(todo.getDate());
+
+        // Set up the delete button click listener
+        holder.deleteButton.setOnClickListener(v -> onDeleteClickListener.onDeleteClick(todo.getId()));
+    }
 }
