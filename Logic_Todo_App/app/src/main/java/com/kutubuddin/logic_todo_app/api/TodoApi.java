de049973c7205fc9490bfae715dfd7d85524690f
Md Kutub Uddin <mdkutub150@gmail.com>
2024-12-27 19:14:11 +0600

    Update TodoApi.java

--- Logic_Todo_App/app/src/main/java/com/kutubuddin/logic_todo_app/api/TodoApi.java
+++ Logic_Todo_App/app/src/main/java/com/kutubuddin/logic_todo_app/api/TodoApi.java
@@ -1,19 +1,20 @@
 package com.kutubuddin.logic_todo_app.api;
 
-
-
 import com.kutubuddin.logic_todo_app.model.TodoModel;
 
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface TodoApi {
 
-    // GET request to view   Todo View
+    // GET request to view all Todos
     @GET("todoapps")
     Call<List<TodoModel>> getTodo();
 
@@ -21,5 +22,11 @@ public interface TodoApi {
     @POST("todoapps")
     Call<Void> createTodo(@Body TodoModel todo);
 
+    // DELETE request to delete a Todo by ID
+    @DELETE("todoapps/{id}")
+    Call<Void> deleteTodo(@Path("id") int id);
 
+    // PUT request to update a Todo by ID
+    @PUT("todoapps/{id}")
+    Call<Void> updateTodo(@Path("id") int id, @Body TodoModel todo);
 }
